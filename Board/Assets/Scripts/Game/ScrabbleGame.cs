3325f3295d3b3ea53c2b6a67994a1da00d21719d
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-10-12 09:52:18 -0600

    Adding board base code.

---
+++ Board/Assets/Scripts/Game/ScrabbleGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrabbleGame
+{
+    #region Methods
+
+    void Awake()
+    {
+        ScrabbleLogicManager.Instance.Initialize();
+    }
+
+    #endregion
+}
--- Board/Assets/Scripts/Logic/ScrabbleLogicManager.cs
+++ Board/Assets/Scripts/Logic/ScrabbleLogicManager.cs
@@ -9,5 +9,38 @@ public class ScrabbleLogicManager
     public DeckData deck                = new DeckData();
     public BoardData board              = new BoardData();
 
+    private static ScrabbleLogicManager _instance = null;
+
     #endregion
+    
+    #region Properties
+
+    public static ScrabbleLogicManager Instance
+    {
+        get 
+        {
+            if(_instance != null)
+                return _instance;
+
+            _instance = new ScrabbleLogicManager();
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    private ScrabbleLogicManager()
+    {
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+    #endregion
+
+
 }
