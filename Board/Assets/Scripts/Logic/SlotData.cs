0e683b234faab32185521bfa1206de9037a151b6
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-10-12 11:24:26 -0600

    Adding board base code.

--- Board/Assets/Scripts/Logic/SlotData.cs
+++ Board/Assets/Scripts/Logic/SlotData.cs
@@ -5,7 +5,37 @@ public class SlotData
 {
     #region Variables
 
-    private ChipData chip = null;
+    private ChipData _chip       = null;
+    private bool _isTemporalChip = false;
+
+    #endregion
+
+    #region Properties
+
+    public ChipData Chip
+    {
+        get { return _chip; }
+        set
+        {
+            _chip           = value;
+            _isTemporalChip = true;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void Commit()
+    {
+        _isTemporalChip = false;
+    }
+
+    public void Revert()
+    {
+        _chip           = null;
+        _isTemporalChip = false;
+    }
 
     #endregion
 }
